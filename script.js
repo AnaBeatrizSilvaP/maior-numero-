9265ce28cdc5d1d0196658574093fa2968519b7e
AnaBeatrizSilvaP <ana.b.pinto6@aluno.senai.br>
2024-05-10 15:27:44 -0500

    subir arquivo

---
+++ script.js
@@ -0,0 +1,8 @@
+var num1=(prompt=("insira um numero"))
+var num2=(prompt=("insira um numero"))
+var num3=(prompt=("insira um numero"))
+function EncontrarMaiorNumero (num1, num2, num3){
+    if (num1>num2 && num2<num3){
+    alert("Você encontrou o maior numero")
+    }
+}
